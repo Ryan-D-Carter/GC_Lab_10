8ac603d1323a2badde355f638d5e3bfa01f708ec
Ryan-D-Carter <RyanCarterMI@gmail.com>
2021-05-04 18:15:35 -0400

    Completed Lab 10

---
+++ GC_Lab_10/Movie.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GC_Lab_10
+{
+    class Movie
+    {
+        //fields
+        private string title;
+        private string category;
+        private string index;
+
+        //properties
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+            set
+            {
+                category = value;
+            }
+        }
+
+        public string Index
+        {
+            get
+            {
+                return index;
+            }
+            set
+            {
+                index = value;
+            }
+        }
+        //constructors
+        public Movie()
+        {
+
+        }
+
+        public Movie(string _title, string _category, string _index)
+        {
+            title = _title;
+            category = _category;
+            index = _index;
+        }
+
+        //methods
+
+
+    }
+}
