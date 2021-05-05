c544115163187f75c062bfa2ff3ab2390643d037
Ryan-D-Carter <RyanCarterMI@gmail.com>
2021-05-04 20:13:31 -0400

    Completed Lab 10

--- GC_Lab_10/Movie.cs
+++ GC_Lab_10/Movie.cs
@@ -62,6 +62,48 @@ namespace GC_Lab_10
 
         //methods
 
+        public static void PromptUser()
+        {
+
+            Movie aladdin = new Movie("Aladdin", "animated", "1");
+            Movie brave = new Movie("Brave", "animated", "1");
+            Movie lionKing = new Movie("The Lion King", "animated", "1");
+            Movie findingNemo = new Movie("Finding Nemo", "animated", "1");
+            Movie godfather = new Movie("The Godfather", "drama", "2");
+            Movie citizenKane = new Movie("Citizen Kane", "drama", "2");
+            Movie halloween = new Movie("Halloween", "horror", "3");
+            Movie alien = new Movie("Alien", "horror", "3");
+            Movie warWorlds = new Movie("War of the Worlds", "scifi", "4");
+            Movie bladeRunner = new Movie("Blade Runner", "scifi", "4");
+            var movies = new List<Movie>() { aladdin, brave, godfather, citizenKane, halloween, alien, warWorlds, bladeRunner, lionKing, findingNemo };
+            movies.Sort((x, y) => x.Title.CompareTo(y.Title));
+
+            Console.WriteLine("What category are you interest in? (1 - Animated, 2 - Drama, 3 - Horror, 4 - Scifi)");
+                var input = Console.ReadLine().ToLower();
+
+
+                switch (input)
+                {
+                    case "animated":
+                    case "drama":
+                    case "horror":
+                    case "scifi":
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                        foreach (var movie in movies)
+                        {
+                            if (movie.Category == input || movie.Index == input)
+                                Console.WriteLine(movie.Title);
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("That is not a valid category");
+                        break;
+                }
+        }
 
     }
 }
--- GC_Lab_10/Program.cs
+++ GC_Lab_10/Program.cs
@@ -8,51 +8,18 @@ namespace GC_Lab_10
     {
         static void Main(string[] args)
         {
-            //movies
-            Movie Aladdin = new Movie("Aladdin", "animated", "1");
-            Movie Brave = new Movie("Brave", "animated", "1");
-            Movie lionKing = new Movie("The Lion King", "animated", "1");
-            Movie findingNemo = new Movie("Finding Nemo", "animated", "1");
-            Movie godfather = new Movie("The Godfather", "drama", "2");
-            Movie citizenKane = new Movie("Citizen Kane", "drama", "2");
-            Movie halloween = new Movie("Halloween", "horror", "3");
-            Movie alien = new Movie("Alien", "horror", "3");
-            Movie warWorlds = new Movie("War of the Worlds", "scifi", "4");
-            Movie bladeRunner = new Movie("Blade Runner", "scifi", "4");
-            var movies = new List<Movie>() { Aladdin, Brave, godfather, citizenKane, halloween, alien, warWorlds, bladeRunner, lionKing, findingNemo };
-            movies.Sort((x, y) => x.Title.CompareTo(y.Title));
+            
+            
 
             Console.WriteLine("Welcome to the Movie List Application!");
             Console.WriteLine($"\nThere are 10 movies in this list.");
 
+            
+
             bool repeat = true;
             while (repeat)
             {
-                Console.WriteLine("What category are you interest in? (1 - Animated, 2 - Drama, 3 - Horror, 4 - Scifi)");
-                var input = Console.ReadLine().ToLower();
-
-
-                switch (input)
-                {
-                    case "animated":
-                    case "drama":
-                    case "horror":
-                    case "scifi":
-                    case "1":
-                    case "2":
-                    case "3":
-                    case "4":
-                        foreach (var movie in movies)
-                        {
-                            if (movie.Category == input || movie.Index == input)
-                                Console.WriteLine(movie.Title);
-                        }
-                        break;
-
-                    default:
-                        Console.WriteLine("That is not a valid category");
-                        break;
-                }
+                Movie.PromptUser();
 
                 //continue?
                 bool conLoop = true;
